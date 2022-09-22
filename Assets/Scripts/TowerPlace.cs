734b1b7079d91ccfd8e35e630e06ac1cb796a92f
Masenkyo <33390@ma-web.nl>
2022-09-22 13:13:32 +0200

    fix

--- Assets/Scripts/TowerPlace.cs
+++ Assets/Scripts/TowerPlace.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class TowerPlace : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject NormalTower;
+    public GameObject Tower;
+    private Camera cam => Camera.main.GetComponent<Camera>();
+    Vector3 mousepos => new Vector3((Mathf.Floor(cam.ScreenToWorldPoint(Input.mousePosition).x)) + 0.5f, (Mathf.Floor(cam.ScreenToWorldPoint(Input.mousePosition).y)) + 0.5f, 0);
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void LateUpdate()
     {
-        
+        if (Tower != null)
+        Tower.transform.position = mousepos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Tower = null;
+            //if (GameObject.FindObjectsOfType<GameObject>().Any(c => c.tag == "Tower" && c == Tower))
+            //{
+                
+            //}
+            
+        }
     }
+
+    public void TowerPlacee()
+    {
+        Tower
+                 = Instantiate(NormalTower, mousepos, transform.rotation);
+
+    }
+    
 }
